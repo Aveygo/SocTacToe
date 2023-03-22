1ab3619ce1ad3045cd5ef37d037bf1c5fb3ab83f
Aveygo <48315552+Aveygo@users.noreply.github.com>
2023-03-22 09:26:32 +0000

    Update test.py
    Fixes, added 500 response

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 # Simple http server / file requestor with python sockets
-import socket
+import socket, os
 
 # Create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Get local machine name and port
 host, port = "0.0.0.0", 80
-port = 80
 
 # Bind to the port
 s.bind((host, port))
@@ -14,9 +13,9 @@ s.bind((host, port))
 # Now wait for client connection.
 s.listen(5)
 
-while True:
-    print('Ready to serve...')
+print('Listening...')
 
+while True:
     # Establish connection with client.
     c, addr = s.accept()
 
@@ -25,38 +24,39 @@ while True:
 
     try:
         # Parse data to get file name
-        content_path = data.split()[1].decode()[1:]
-        filename = content_path
+        filename = data.split()[1].decode()[1:]
 
+        # Common practice to return index.html if no file is specified
         if filename == "":
-            filename = "simpleWeb.html"
+            filename = "index.html"
 
-        print("Requesting file: " + filename)
+        # Check if file exists
+        if not os.path.exists(filename):
+            c.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        else:
+            
+            print("Requesting file: " + filename)
 
-        mimetype = "text/plain"
-        if filename.endswith(".html"):
-            mimetype = "text/html"
+            # Set mimetype based on file extension
+            mimetype = "text/" + filename.split(".")[-1]
 
-        # Open file
-        data = open(filename, "rb").read()
+            # Open file
+            data = open(filename, "rb").read()
 
-        # Send HTTP header into socket
-        c.send(b"HTTP/1.1 200 OK\r\n")
-        c.send(b"Content-Type: " + mimetype.encode() + b"\r\n")
-        c.send(b"\r\n")
+            # Send HTTP header into socket
+            c.send(b"HTTP/1.1 200 OK\r\n")
+            c.send(b"Content-Type: " + mimetype.encode() + b"\r\n")
+            c.send(b"\r\n")
 
-        # Send the content of the requested file to the client
-        c.send(data)
-        
-        # End line
-        c.send(b"\r\n")
+            # Send the content of the requested file to the client
+            c.send(data)
+            
+            # End response
+            c.send(b"\r\n")
 
     except Exception as e:
         # return 404 error
-        c.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        c.send(b"HTTP/1.1 500 Internal Server Error\r\n\r\n")
     
     # Close connection
     c.close()
-
-    # Close server
-    # sys.exit()
